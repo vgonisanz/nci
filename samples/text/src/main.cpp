533a41c443a0b3966cb5a5d8fa4ecc500d45f04c
vgonisanz <vgonisanz@gmail.com>
2019-08-12 22:17:33 +0200

    sample: text: Added basic text sample. TODO add frame container

--- samples/text/src/main.cpp
+++ samples/text/src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "imanager.h"
+#include "textFrame.h"
 #include "frame.h"
 
 /**
@@ -12,17 +13,22 @@ bool run_frontend()
 	nci::IManager manager;
 	nci::Size2D screen_size = manager.get_size();
 
-	nci::Point2D origin_1(0, 0);
-	nci::Size2D size_1(screen_size.width, screen_size.height);
+	/* Create a text label */
+	nci::Point2D origin_text_1(1, 1);
+	nci::Size2D size_text_1(20, 1);
+	std::shared_ptr<nci::TextFrame> textframe_1(new nci::TextFrame("TextFrame_1", origin_text_1, size_text_1));
+	textframe_1->set_text("Hello world!");
+
+	nci::Point2D origin_1(screen_size.width/2, screen_size.height/2);
+	nci::Size2D size_1(screen_size.width/2, screen_size.height/2);
 	std::shared_ptr<nci::Frame> frame_1(new nci::Frame("Frame_1", origin_1, size_1));
 	frame_1->set_background_color(3);
 
-	/* Create a text label */
-	// TODO
 	/* Create a editable text */
 	// TODO
 
-	manager.add_frame(std::shared_ptr<nci::Frame>(frame_1));
+	manager.add_frame(std::static_pointer_cast<nci::VirtualFrame>(textframe_1));
+	manager.add_frame(std::static_pointer_cast<nci::VirtualFrame>(frame_1));
 	return manager.run();
 }
 
