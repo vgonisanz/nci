2b3940d370a57c7618d86b6a3a81c4784595d9fb
vgonisanz <vgonisanz@gmail.com>
2019-09-08 16:04:47 +0200

    samples: text_in_frames: Simplified positions

--- samples/text/in_frames/src/main.cpp
+++ samples/text/in_frames/src/main.cpp
@@ -27,7 +27,7 @@ bool run_frontend()
 	frame_1->set_background_color(3);
 
 	/* Create a children text for red frame */
-	nci::Point2D origin_text_1(1, 1);
+	nci::Point2D origin_text_1(0, 0);
 	nci::Size2D size_text_1(20, 1);
 	std::shared_ptr<nci::TextFrame> textframe_1(new nci::TextFrame("TextFrame_1", origin_text_1, size_text_1));
 	textframe_1->set_text("I'm children 1");
@@ -39,7 +39,7 @@ bool run_frontend()
 	frame_2->set_background_color(4);
 
 	/* Create a children text for blue frame */
-	nci::Point2D origin_text_2(1, 1);
+	nci::Point2D origin_text_2(0, 0);
 	nci::Size2D size_text_2(20, 1);
 	std::shared_ptr<nci::TextFrame> textframe_2(new nci::TextFrame("TextFrame_2", origin_text_2, size_text_2));
 	textframe_2->set_text("I'm children 2");
@@ -51,13 +51,10 @@ bool run_frontend()
 	frame_3->set_background_color(5);
 
 	/* Create a children text for green frame */
-	nci::Point2D origin_text_3(1, 1);
+	nci::Point2D origin_text_3(0, 0);
 	nci::Size2D size_text_3(20, 1);
 	std::shared_ptr<nci::TextFrame> textframe_3(new nci::TextFrame("TextFrame_3", origin_text_3, size_text_3));
 	textframe_3->set_text("I'm children 3");
-
-	/* Create a editable text */
-	// TODO
 
 	manager.add(textframe_0);
 	manager.add(frame_1);
@@ -81,8 +78,6 @@ int main()
 	{
 		nci::tear_down_ncurses();
 	}
-	/* Create a thread to change data after a while */
-	// TODO
 
 	if (!result)
 		std::cout << "Could not run manager" << std::endl;
