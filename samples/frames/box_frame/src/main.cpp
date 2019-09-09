f0423c9c9d610e44f95089b7d1ebac182a4f4f16
vgonisanz <vgonisanz@gmail.com>
2019-09-09 20:45:21 +0200

    samples: Added sample to draw 1 unique box

---
+++ samples/frames/box_frame/src/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+#include "nci.h"
+#include "imanager.h"
+#include "frame.h"
+
+/**
+ * Create a frame that use whole screen
+ */
+bool run_frontend()
+{
+	/* First create manager to initialize ncurse, handle stdout and windows */
+	nci::IManager manager;
+	nci::Size2D screen_size = manager.get_size();
+	std::cout << "screen_size: " << screen_size.width << ", " << screen_size.height << std::endl;
+
+	nci::Point2D origin_1(0, 0);
+	nci::Size2D size_1(screen_size.width, screen_size.height);
+	std::shared_ptr<nci::Frame> frame_1(new nci::Frame("Frame_1", origin_1, size_1));
+	frame_1->set_background_color(3);
+	frame_1->set_border(true);	/* true by default */
+
+	manager.add(frame_1);
+	return manager.run();
+}
+
+int main()
+{
+	bool result = false;
+
+ 	try
+	{
+		result = run_frontend();
+	}
+	catch (const std::exception& e)
+	{
+		nci::tear_down_ncurses();
+	}
+
+	if (!result)
+		std::cout << "Could not run manager" << std::endl;
+
+	std::cout << "End of execution" << std::endl;
+	return 0;
+}
