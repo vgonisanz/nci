6f82db63b40aedcb68e73369f21a3d7f93bad66e
vgonisanz <vgonisanz@gmail.com>
2019-09-10 18:16:46 +0200

    samples: constraint: Modified sample to check frame with more size with and without border

--- samples/frames/constraint_children/src/main.cpp
+++ samples/frames/constraint_children/src/main.cpp
@@ -18,30 +18,66 @@ bool run_frontend()
 	nci::IManager manager;
 	nci::Size2D screen_size = manager.get_size();
 
-	nci::Point2D origin_1(0, 0);
-	nci::Size2D size_1(screen_size.width, screen_size.height/2);
-	std::shared_ptr<nci::Frame> frame_1(new nci::Frame("frame_1", origin_1, size_1));
-	frame_1->set_background_color(3);
+	/* TOP-LEFT frames with box - Inside frame is smaller than parent frame */
+	nci::Point2D origin_top_left(0, 0);
+	nci::Size2D size_top_left(screen_size.width/2, screen_size.height/2);
+	std::shared_ptr<nci::Frame> frame_top_left(new nci::Frame("frame_top_left", origin_top_left, size_top_left));
+	frame_top_left->set_background_color(3);
 
-	nci::Point2D origin_1_1(0, 0);
-	nci::Size2D size_1_1(screen_size.width - 2, screen_size.height/2 - 2);
-	std::shared_ptr<nci::Frame> frame_1_1(new nci::Frame("frame_1_1", origin_1_1, size_1_1));
-	frame_1_1->set_background_color(3);
+	nci::Point2D origin_top_left_in(0, 0);
+	nci::Size2D size_top_left_in(size_top_left.width - 2, size_top_left.height - 2);
+	std::shared_ptr<nci::Frame> frame_top_left_in(new nci::Frame("frame_top_left_in", origin_top_left_in, size_top_left_in));
+	frame_top_left_in->set_background_color(3);
 
-	nci::Point2D origin_2(0, screen_size.height/2);
-	nci::Size2D size_2(screen_size.width, screen_size.height/2);
-	std::shared_ptr<nci::Frame> frame_2(new nci::Frame("Frame_2", origin_2, size_2));
-	frame_2->set_background_color(4);
+	/* TOP-RIGHT frames without box - Inside frame is smaller than parent frame */
+	nci::Point2D origin_top_right(size_top_left.width, 0);
+	nci::Size2D size_top_right(size_top_left);
+	std::shared_ptr<nci::Frame> frame_top_right(new nci::Frame("Frame_top_right", origin_top_right, size_top_right));
+	frame_top_right->set_background_color(4);
+	frame_top_right->set_border(false);
 
-	nci::Point2D origin_2_1(0, 0);
-	nci::Size2D size_2_1(screen_size.width, screen_size.height/2);
-	std::shared_ptr<nci::Frame> frame_2_1(new nci::Frame("frame_2_1", origin_2_1, size_2_1));
-	frame_2_1->set_background_color(4);
+	nci::Point2D origin_top_right_in(0, 0);
+	nci::Size2D size_top_right_in(size_top_left - 1);
+	std::shared_ptr<nci::Frame> frame_top_right_in(new nci::Frame("frame_top_right_in", origin_top_right_in, size_top_right_in));
+	frame_top_right_in->set_background_color(3);
+	frame_top_right_in->set_border(false);
 
-	manager.add(frame_1);
-	manager.add(frame_2);
-	frame_1->add(frame_1_1);
-	frame_2->add(frame_2_1);
+	/* BOT-LEFT frames with box - Inside frame has same size than parent frame */
+	nci::Point2D origin_bot_left(0, size_top_left.height);
+	nci::Size2D size_bot_left(screen_size.width/2, screen_size.height/2);
+	std::shared_ptr<nci::Frame> frame_bot_left(new nci::Frame("frame_bot_left", origin_bot_left, size_bot_left));
+	frame_bot_left->set_background_color(5);
+
+	nci::Point2D origin_bot_left_in(0, 0);
+	nci::Size2D size_bot_left_in(size_bot_left.width - 2, size_bot_left.height - 2);
+	std::shared_ptr<nci::Frame> frame_bot_left_in(new nci::Frame("frame_bot_left_in", origin_bot_left_in, size_bot_left_in));
+	frame_bot_left_in->set_background_color(5);
+
+	/* BOT-RIGHT frames without box - Inside frame has same size than parent frame */
+	nci::Point2D origin_bot_right(size_bot_left.width, size_top_left.height);
+	nci::Size2D size_bot_right(size_bot_left);
+	std::shared_ptr<nci::Frame> frame_bot_right(new nci::Frame("frame_bot_right", origin_bot_right, size_bot_right));
+	frame_bot_right->set_background_color(6);
+	frame_bot_right->set_border(false);
+
+	nci::Point2D origin_bot_right_in(0, 0);
+	nci::Size2D size_bot_right_in(size_bot_left - 1);
+	std::shared_ptr<nci::Frame> frame_bot_right_in(new nci::Frame("frame_bot_right_in", origin_bot_right_in, size_bot_right_in));
+	frame_bot_right_in->set_background_color(2);
+	frame_bot_right_in->set_border(false);
+
+	manager.add(frame_top_left);
+	frame_top_left->add(frame_top_left_in);
+
+	manager.add(frame_top_right);
+	frame_top_right->add(frame_top_right_in);
+
+	manager.add(frame_bot_left);
+	frame_bot_left->add(frame_bot_left_in);
+
+	manager.add(frame_bot_right);
+	frame_bot_right->add(frame_bot_right_in);
+
 	return manager.run();
 }
 
