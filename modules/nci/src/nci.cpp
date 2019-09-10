fb82a117f0e8ee36ccf006c256c5fda3a11c8550
vgonisanz <vgonisanz@gmail.com>
2019-09-10 20:49:29 +0200

    nci: changed colors in init method

--- modules/nci/src/nci.cpp
+++ modules/nci/src/nci.cpp
@@ -20,16 +20,20 @@ WINDOW* initialize_ncurses()
     curs_set(CURSOR::INVISIBLE);
     if (has_colors())
     {
+        use_default_colors();
         start_color();                  /* Allow color */
-        //use_default_colors();           /* Default colors */
         if (can_change_color())         // TODO pallete colors
         {
-            init_pair(1, COLOR_RED, COLOR_BLACK);
-            init_pair(2, COLOR_CYAN, COLOR_BLACK);
+            init_pair(1, COLOR_WHITE, COLOR_WHITE);
+            init_pair(2, COLOR_WHITE, COLOR_BLACK);
             init_pair(3, COLOR_WHITE, COLOR_RED);
-            init_pair(4, COLOR_WHITE, COLOR_BLUE);
-            init_pair(5, COLOR_WHITE, COLOR_GREEN);
-            init_pair(6, COLOR_WHITE, COLOR_MAGENTA);
+            init_pair(4, COLOR_WHITE, COLOR_GREEN);
+            init_pair(5, COLOR_WHITE, COLOR_YELLOW);
+            init_pair(6, COLOR_WHITE, COLOR_BLUE);
+            init_pair(7, COLOR_WHITE, COLOR_MAGENTA);
+            init_pair(8, COLOR_WHITE, COLOR_CYAN);
+            init_pair(9, COLOR_WHITE, COLOR_MAGENTA);
+            init_pair(10, COLOR_WHITE, COLOR_MAGENTA);
         }
     }
     return general;
