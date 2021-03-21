2dfc86b2866647ec0d6aa7187d73ef580c7de280
vgonisanz <vgonisanz@gmail.com>
2021-03-21 09:30:57 +0100

    tests: Added sample to use tests

--- sources/tests/tests_base.cpp
+++ sources/tests/tests_base.cpp
@@ -1,6 +1,15 @@
+#include <string>
 #include "gtest/gtest.h"
 
 TEST(FirstTest, Test)
 {
-    EXPECT_EQ(1, 1);
+    bool expected = true;
+    bool got = true;
+    EXPECT_EQ(got, expected);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
